0f87a29973fd9e89cf38a38d2c8bd2b517ae8ecd
Moringa School <fatmanamnyak22@yahoo.com>
2022-09-23 09:19:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+// Write your code in this file
+const currentUser = 'Fatma';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, 'FATMA!' ${currentUser}!` ;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` ;
